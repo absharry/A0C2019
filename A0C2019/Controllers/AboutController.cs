88d623ea997af0331182f2e097c4c373f09c7d2f
Adam Prentice <adamjlprentice@gmail.com>
2019-12-02 21:44:46 +0000

    Initial Api build

---
+++ A0C2019/Controllers/AboutController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdventOfCode.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AboutController : ControllerBase
+    {
+    }
+}
---
+++ A0C2019/Controllers/Year2019Controller.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdventOfCode.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/2019")]
+    [ApiController]
+    public class Year2019Controller : ControllerBase
+    {
+        public Year2019Controller()
+        {
+
+        }
+
+        [HttpGet]
+        [Route("1")]
+        public IActionResult Day1()
+        {
+            return new OkObjectResult("Hello!");
+        }
+    }
+}
